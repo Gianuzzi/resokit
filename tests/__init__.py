4f69e868cc14619738730b2205128357e55f5735
Gianuzzi <ema.gianuzzi@gmail.com>
2024-10-15 13:02:53 -0300

    Add tests module and test_datasets.py file

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   ResoKit Project (https://github.com/Gianuzzi/resokit).
+# Copyright (c) 2024, Emmanuel Gianuzzi
+# License: MIT
+#   Full Text: https://github.com/Gianuzzi/resokit/blob/master/LICENSE
+
+# This file indicates that the directory should be treated as a package.
+
+# ============================================================================
+# DOCS
+# ============================================================================
+
+"""The tests module includes tools for testing the ResoKit package."""
---
+++ tests/test_datasets.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   ResoKit Project (https://github.com/Gianuzzi/resokit).
+# Copyright (c) 2024, Emmanuel Gianuzzi
+# License: MIT
+#   Full Text: https://github.com/Gianuzzi/resokit/blob/master/LICENSE
+
+# ============================================================================
+# IMPORTS
+# ============================================================================
