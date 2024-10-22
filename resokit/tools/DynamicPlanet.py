2ef648920cad4784568251cc2f3221abbeac6a90
MatiasCerioni <matias.cerioni@unc.edu.ar>
2024-10-22 10:05:17 -0300

    fix things

--- resokit/tools/DynamicPlanet.py
+++ resokit/tools/DynamicPlanet.py
@@ -1,113 +1,109 @@
 import attrs
 import numpy as np
-import uttr
-from typing import List
+
+_SEQUENCE_TYPES = (list, tuple, np.ndarray)
+
+_TIME_SERIES_KWARGS = {
+    "validator": attrs.validators.instance_of(_SEQUENCE_TYPES),
+    "converter": np.asarray,
+    "default": [np.nan],
+}
+
+_PLNAME_KWARGS = {
+    "validator": attrs.validators.instance_of(str),
+    "default": "",
+}
+
+_CONSTANT_KWARGS = {
+    "validator": attrs.validators.instance_of((int, float)),
+    "converter": float,
+    "default": np.nan,
+}
+
+_BOOL_KWARGS = {
+    "validator": attrs.validators.instance_of(bool),
+    "default": False,
+}
+
+_INTEGER_KWARGS = {
+    "validator": attrs.validators.instance_of(int),
+    "default": 0,
+}
 
 
 @attrs.define()
 class DynamicPlanet:
-    """
-    Time series for the evolution of a planet
-    """
+    """Time series for the evolution of a planet"""
 
-    name_kw = {
-        "validator": attrs.validators.instance_of(str),
-        "default": '',
-    }
-
-    timeSeries_kw = {
-        "validator": attrs.validators.instance_of((list, np.ndarray)),
-        "converter": np.asarray,
-        "default": [np.nan],
-    }
-
-    constant_kw = {
-        "validator": attrs.validators.instance_of((int,float)),
-        "converter": float,
-        "default": np.nan,
-    }
-    
-    bool_kw = {
-        "validator": attrs.validators.instance_of(bool),
-        "default": False,
-    }
-    
-    
     # input parameters
-    name: str = attrs.field(**name_kw)
+    name: str = attrs.field(**_PLNAME_KWARGS)
 
-    t: list = attrs.field(**timeSeries_kw)  # days
-    a: list = attrs.field(**timeSeries_kw)  # AU
-    e: list = attrs.field(**timeSeries_kw)  #
-    inc: list = attrs.field(**timeSeries_kw)  # deg
-    M: list = attrs.field(**timeSeries_kw)  # deg
-    w: list = attrs.field(**timeSeries_kw)  # deg
-    Omega: list = attrs.field(**timeSeries_kw)  # deg
+    times: list = attrs.field(**_TIME_SERIES_KWARGS)  # days
+    a: list = attrs.field(**_TIME_SERIES_KWARGS)  # AU
+    e: list = attrs.field(**_TIME_SERIES_KWARGS)  #
+    inc: list = attrs.field(**_TIME_SERIES_KWARGS)  # deg
+    M: list = attrs.field(**_TIME_SERIES_KWARGS)  # deg
+    w: list = attrs.field(**_TIME_SERIES_KWARGS)  # deg
+    Omega: list = attrs.field(**_TIME_SERIES_KWARGS)  # deg
 
-    mass: float = attrs.field(**constant_kw)  # earth masses
-    radius: float = attrs.field(**constant_kw)  # earth radii
-    
-    is_star : bool = attrs.field(**bool_kw)
+    mass: float = attrs.field(**_CONSTANT_KWARGS)  # earth masses
+    radius: float = attrs.field(**_CONSTANT_KWARGS)  # earth radii
+
+    is_star: bool = attrs.field(**_BOOL_KWARGS)
+
 
 @attrs.define()
 class Star:
     """
     Star of the system
+
     """
-    
-    constant_kw = {
-        "validator": attrs.validators.instance_of((int,float)),
-        "converter": float,
-        "default": np.nan,
-    }
-    
-    mass: float = attrs.field(**constant_kw)  # solar masses
-    radius: float = attrs.field(**constant_kw)  # solar radii
-    teff: float = attrs.field(**constant_kw) # kelvin
-    met: float = attrs.field(**constant_kw) 
+
+    mass: float = attrs.field(**_CONSTANT_KWARGS)  # solar masses
+    radius: float = attrs.field(**_CONSTANT_KWARGS)  # solar radii
+    teff: float = attrs.field(**_CONSTANT_KWARGS)  # kelvin
+    met: float = attrs.field(**_CONSTANT_KWARGS)
+
+
+_STAR_OBJECT_KWARGS = {
+    "validator": attrs.validators.instance_of(Star),
+    "default": Star(),  # empty star
+}
+
+_PLANET_OBJECT_KWARGS = {
+    "validator": attrs.validators.instance_of((list, tuple, np.ndarray)),
+    "default": [np.nan],
+}
 
 
 @attrs.define()
 class DynamicSystem:
-    """
-    Global properties of the system
-    """
-    
-    int_kw = {
-        "validator": attrs.validators.instance_of(int),
-        "converter": int,
-        "default": np.nan,
-    }
-    
-    star_kw = {
-        "validator": attrs.validators.instance_of(Star),
-        "default": np.nan,
-    }
-    
-    planets_kw = {
-        "validator": attrs.validators.instance_of(list),
-        "default": [np.nan],
-    }
-    
-    npl: int = attrs.field(**int_kw)
-    nstars: int = attrs.field(**int_kw)
-    
-    star: Star = attrs.field(**star_kw)
-    
-    planets: list = attrs.field(**planets_kw)
-    
-    # # validar lista de planetas        
-    # def __attrs_post_init__(self):
-    #     """Validate that the type of each particleset is correct."""
-    #     pset_types = [
-    #         ("star", self.star, Star),
-    #         ("planets", self.planets, list),
-    #     ]
-    #     planets = self.planets
-    #     for planet in planets:
-    #         if not isinstance(planet,DynamicPlanet):
-    #             raise TypeError(f"{planet} must be of type DynamicPlanet")  
-    
-    
-    # sys = DynamicSystem([pl1,pl2])
-    # return(sys)
+    """Global properties of the system"""
+
+    nstars: int = attrs.field(**_INTEGER_KWARGS)
+    star: Star = attrs.field(**_STAR_OBJECT_KWARGS)
+    planets: list = attrs.field(**_PLANET_OBJECT_KWARGS)
+    npl: int = attrs.field(init=False, **_INTEGER_KWARGS)
+
+    def __attrs_post_init__(self):
+        """Validate types of star and planets,
+        AND add calculated attributes.
+
+        """
+        # Validation
+        if not isinstance(self.star, Star):
+            raise TypeError("star must be of type Star")
+
+        for ith, planet in enumerate(self.planets):
+            if not isinstance(planet, DynamicPlanet):
+                raise TypeError(f"planet {ith+1} must be of type DynamicPlanet")
+
+        # Additional calculated attributes
+        self.npl = len(self.planets)
+
+
+pl1 = DynamicPlanet()
+pl2 = DynamicPlanet()
+st1 = Star()
+sys1 = DynamicSystem(planets=[pl1, pl2])
+sys2 = DynamicSystem(planets=[pl1, pl2, pl1])
